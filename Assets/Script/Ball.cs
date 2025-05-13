f41d9555609dd0d5b9c4d9dc583904ac9d35db7a
Robby Kurniawan <rdata088@gmail.com>
2025-05-13 20:27:46 +0700

    Update Ball.cs

--- Assets/Script/Ball.cs
+++ Assets/Script/Ball.cs
@@ -9,6 +9,7 @@ public class Ball : MonoBehaviour
     public bool isBounce;
     public bool bonusGoal;
     public bool isLastHit1;
+    
     // Start is called before the first frame update
     void Start()
     {
